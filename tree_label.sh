96b79192ceeddba83daedd53aafb1df0895a0b89
Marco Ponzi <marco.ponzi@gmail.com>
2019-07-17 11:09:49 +0200

    sh script renamed

--- tree_label.sh
+++ tree_label.sh
@@ -1,12 +1,10 @@
+rm /tmp/img1000.png /tmp/caption1.jpg /tmp/merge.png
 convert -geometry 950 $1 /tmp/img1000.png
-#iwidth=`identify -format '%w' $1`
+
 iwidth=950
 
 lab=` echo $1 | sed -e '1,$s/\....$//' | sed -e 's/_/ /g'`
 
-#convert -font "DroidSansB" -pointsize 18 -size $iwidth'x24' -gravity North caption:"$lab" /tmp/caption1.jpg
-#convert -bordercolor white -border 2x2 tmpcaption1.jpg tmpcaption.jpg
-#convert -font "DroidSansB" -pointsize 24 -size $iwidth'x40' -gravity Center caption:"$lab" /tmp/caption1.jpg
 convert  -pointsize 20 -size $iwidth'x50' -gravity Center caption:"$lab" /tmp/caption1.jpg
 
 montage -geometry +0+0 -tile 1x2 /tmp/img1000.png /tmp/caption1.jpg /tmp/merge.png
--- trees.sh
+++ trees.sh
@@ -3,13 +3,15 @@ for w in $*
 do
 for gramm in grammar/EMS_edited.lark  grammar/Stolfi_reduced.lark
 do
-gname=`echo $gramm | sed -e 's%.*/%%' | sed -e 's/_.*//'`
-echo $gname
-echo $w | python tree_img.py $gramm 
-source ./label_img.sh '__'$w'_'$gname'.png'
-rm '__'$w'_'$gname'.png'
-mv *PNG images
+  gname=`echo $gramm | sed -e 's%.*/%%' | sed -e 's/_.*//'`
+  echo $gname
+  echo $w | python tree_img.py $gramm 
+  source ./tree_label.sh $w'_'$gname'.png'
+  rm $w'_'$gname'.png'
+
 done
+montage -geometry +0+0 -tile 1x2 $w'_Stolfi'*'.PNG' $w'_EMS'*'.PNG' $w'_BOTH.PNG' 
+mv *PNG images
 done
 
 

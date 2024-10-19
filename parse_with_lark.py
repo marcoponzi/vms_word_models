73e26064e1d5772640f803e5ed1e9f5d4b2ca9ba
Marco Ponzi <marco.ponzi@gmail.com>
2024-10-19 12:40:44 +0200

    updated for python3

--- parse_with_lark.py
+++ parse_with_lark.py
@@ -1,10 +1,10 @@
 import sys
-from lark import Lark
+import lark
 
          
 f = open(sys.argv[1], 'r')
 
-lrk = Lark(f)
+lrk = lark.Lark(f)
 
 tot=0
 success=0
@@ -15,25 +15,25 @@ def frmt(myfloat):
 for line in sys.stdin:
   line=line.rstrip()
   if (len(line)>0):
-          count=1
-	  items=line.split()
-	  word=items[0]
-	  if (len(items)>1):
-	    if (items[0].isdigit()):
-	      count=items[0]
-	      word=items[len(items)-1]
-	    else:
-	      count=items[len(items)-1]
-	  tot+=int(count)
-	  print("'"+line+"'")
-	  try:
-	    tree=lrk.parse(word)
-	    sys.stdout.write("SUCCESS:True " + word+" ")
-	    print(repr(tree).replace("Tree",""))
-	    success+=int(count)
-	  except:
-	    print("SUCCESS:False  " + line)
-	    #lrk.parse(word)
+      count=1
+      items=line.split()
+      word=items[0]
+      if (len(items)>1):
+        if (items[0].isdigit()):
+          count=items[0]
+          word=items[len(items)-1]
+        else:
+          count=items[len(items)-1]
+      tot+=int(count)
+      print("'"+line+"'")
+      try:
+        tree=lrk.parse(word)
+        sys.stdout.write("SUCCESS:True " + word+" ")
+        print(repr(tree).replace("Tree",""))
+        success+=int(count)
+      except:
+        print("SUCCESS:False  " + line)
+        #lrk.parse(word)
 
 perc_succ=100.0*float(success)/float(tot)
 
--- tree_img.py
+++ tree_img.py
@@ -22,28 +22,28 @@ def frmt(myfloat):
 for line in sys.stdin:
   line=line.rstrip()
   if (len(line)>0):
-          count=1
-	  items=line.split()
-	  word=items[0]
-	  if (len(items)>1):
-	    if (items[0].isdigit()):
-	      count=items[0]
-	      word=items[len(items)-1]
-	    else:
-	      count=items[len(items)-1]
-	  tot+=int(count)
-	  print("'"+line+"'")
-	  try:
-	    out_tree=lrk.parse(word)
-	    sys.stdout.write("SUCCESS:True " + word+" ")
-	    print(repr(out_tree).replace("Tree",""))
-	    fname=word+"_"+gname+".png"
-	    print(fname)
-	    tree.pydot__tree_to_png( out_tree, fname)
-	    success+=int(count)
-	  except:
-	    print("Exception  " + line)
-	    #lrk.parse(word)
+      count=1
+      items=line.split()
+      word=items[0]
+      if (len(items)>1):
+        if (items[0].isdigit()):
+          count=items[0]
+          word=items[len(items)-1]
+        else:
+          count=items[len(items)-1]
+      tot+=int(count)
+      print("'"+line+"'")
+      try:
+        out_tree=lrk.parse(word)
+        sys.stdout.write("SUCCESS:True " + word+" ")
+        print(repr(out_tree).replace("Tree",""))
+        fname=word+"_"+gname+".png"
+        print(fname)
+        tree.pydot__tree_to_png( out_tree, fname)
+        success+=int(count)
+      except:
+        print("Exception  " + line)
+        #lrk.parse(word)
 
 perc_succ=100.0*float(success)/float(tot)
 
